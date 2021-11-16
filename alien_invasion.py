28170187408a2b5907ad18779663df3b349f794f
Mujahid <mhmmdalimujahid@gmail.com>
2021-11-16 13:05:00 +0600

    added sittings and ship, and code to display the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,43 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
+
+
+
 class AlienInvasion:
+    """A class for the overall game."""
 
     def __init__(self):
         pygame.init()
+
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_height, self.settings.screen_width))
+        pygame.display.set_caption("Alien Invasion")
+        self.screen.fill(self.settings.bg_color)
+        self.ship = Ship(self)
+
+
+
+    def run_game(self):
+        """The main game loop."""
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            #make th emost recently drawn screen visible.
+            pygame.display.flip()
+            self.ship.blit_me()
+
+
+    
+
+if __name__ == '__main__':
+    
+    ai = AlienInvasion()
+    ai.run_game()
---
+++ settings.py
@@ -0,0 +1,16 @@
+import pygame 
+
+
+class Settings:
+    """A class for ai_game settings"""
+
+    def __init__(self):
+
+        #display settings
+        self.screen_width = 800
+        self.screen_height = 1200
+        self.bg_color = (230,230,230)
+
+        #ship settings
+        self.ship_speed = 1
+        
